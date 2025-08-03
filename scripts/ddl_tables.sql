00792f73a66a4e0eeb03eee41946f83bb8d11746
Macar05 <marufwarsame@gmail.com>
2025-08-04 01:43:11 +0300

    Create ddl_tables.sql

---
+++ scripts/ddl_tables.sql
@@ -0,0 +1,91 @@
+/*
+This script will drop tables if exists and recreate.
+so make usre you have proper backup data before you run the code
+
+*/
+
+if OBJECT_ID('bronze.crm_cust_info', 'U') is not null
+drop table bronze.crm_cust_info;
+
+create table bronze.crm_cust_info(
+cst_id int,
+cst_key nvarchar(50),
+cst_firstname varchar(50),
+cst_lastname varchar(50),
+cst_marterial_status nvarchar(50),
+cst_gndr nvarchar(50),
+cst_create_date date
+
+);
+
+if OBJECT_ID('bronze.crm_prd_info', 'U') is not null
+drop table bronze.crm_prd_info;
+
+create table bronze.crm_prd_info(
+prd_id int,
+prd_key nvarchar(50),
+prd_nm varchar(50),
+prd_cost int,
+prd_line nvarchar(50),
+prd_start_dt datetime,
+prd_end_dt datetime
+
+);
+
+if OBJECT_ID('bronze.crm_sales_details', 'U') is not null
+drop table bronze.crm_sales_details;
+create table bronze.crm_sales_details(
+sls_ord_num nvarchar(50),
+sls_prd_key nvarchar(50),
+sls_cus_id int,
+sls_order_dt int,
+sls_ship_dy int,
+sls_due_dt int,
+sls_sales int,
+sls_quantity int,
+sls_price int
+
+);
+
+
+if OBJECT_ID('bronze.erp_loc_a101', 'U') is not null
+drop table bronze.erp_loc_a101;
+
+create table bronze.erp_loc_a101(
+cid nvarchar(50),
+cntry nvarchar(50),
+
+
+);
+
+
+if OBJECT_ID('bronze.erp_cus_az12', 'U') is not null
+drop table bronze.erp_cus_az12;
+create table bronze.erp_cus_az12(
+cid nvarchar(50),
+bdate date,
+gen nvarchar(50),
+
+
+);
+
+if OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') is not null
+drop table bronze.erp_px_cat_g1v2;
+create table bronze.erp_px_cat_g1v2(
+id nvarchar(50),
+cat nvarchar(50),
+subcat nvarchar(50),
+maintenance nvarchar(50)
+
+
+);
+
+
+
+
+
+
+
+
+
+
